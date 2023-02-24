3bc11fc4f48f1e8dc29e25c6c7aa67d54faec1e5
Test tf tg ga <tftggatst@gmail.com>
2023-02-24 01:29:18 +0000

    fix2

--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ terraform {
 
 resource "google_storage_bucket" "bucket1" {
   name          = "feb2023bucket-1"
-  location      = "US"  
+  location      = "US"
+  project = "strong-bus-371816"  
 }
