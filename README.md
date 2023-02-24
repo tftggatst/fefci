8253324d2728b011b042110aa1ed5bc49e21dbdc
Test tf tg ga <tftggatst@gmail.com>
2023-02-24 01:21:33 +0000

    bucket

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# fefci
+# reacme
---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraformstatenew"
+    prefix = "actions"
+  }
+}
+
+
+resource "google_storage_bucket" "bucket1" {
+  name          = "feb2023bucket-1"
+  location      = "US"  
+}
